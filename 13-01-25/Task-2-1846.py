61f94d89095ba2489ce53de40f8287adfde7c853
KZinoveva <kseniazinoveva608@gmail.com>
2025-01-13 16:42:08 +0200

    ответ: cdba

--- 13-01-25/Task-2-1846.py
+++ 13-01-25/Task-2-1846.py
@@ -4,5 +4,9 @@ for a in 0,1:
         for c in 0,1:
             for d in 0,1:
                 f = ((not a) and (not b)) or (b == c) or d
-                if not f:
+                if not f: #если нужны ложные значения
                     print(a, b, c, d)
+
+                if f: #если нужны истинные значения
+                    print(a, b, c, d)
+                #если в таблице и 1 и 0 то без if пишем вообще
