03f62664d214778824c4345ac6f6aaab4d90cb96
User <kseniazinoveva608@gmail.com>
2025-02-03 17:06:46 +0200

    ответ: 23

---
+++ 03-02-25/Task-15-746.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def f(x):
+    P = 12 <= x <= 26
+    Q = 30 <= x <= 53
+    A = A1 <= x <= A2
+    return (A <= P) or Q
+
+ans = []
+line = [i/ 10 for i in range(0 * 10, 70 * 10 )]
+
+for A1, A2 in combinations( line, 2):
+    if all(f(x) == 1 for x in line ):
+        ans.append(A2-A1)
+print(max(ans))
--- 14-01-25/Task-5-14330.py
+++
@@ -1,6 +0,0 @@
-for n in range(10_000,100_000):
-    r = str(n)
-    u = max(r) + min(r)
-    u = (int(u))**2
-    if
-    print(u)
