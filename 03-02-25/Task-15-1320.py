554521ae262c5e3dfa1edc8447368adcb6fe8fa8
User <kseniazinoveva608@gmail.com>
2025-02-03 18:56:37 +0200

    ответ:20

---
+++ 03-02-25/Task-15-1320.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def f(x):
+    P = 10 <= x <= 25
+    Q = 15 <= x <= 30
+    R = 25 <= x <= 40
+    A = A1 <= x <= A2
+    return  (Q <= (not R)) and A and (not P)
+
+ans =[]
+line = [i/10 for i in range(0*10,60*10)]
+
+for A1,A2 in combinations(line,2):
+    if all(f(x)== 0 for x in line):
+        ans.append(A2-A1)
+print(max(ans))
+
