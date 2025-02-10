646d155f53d4308d70dae9a9b9b5fe63eeda8e33
User <kseniazinoveva608@gmail.com>
2025-02-10 13:41:19 +0200

    ответ: 48

---
+++ 04-02-25/Task-15-16833.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def f(x):
+    P = 25 <= x <= 73
+    Q = 75 <= x <= 118
+    A = A1 <= x <= A2
+    return (A and (not Q)) <= (P or Q)
+
+ans = []
+line = [i/10 for i in range(23*10, 120*10)]
+for A1,A2 in combinations(line, 2):
+    if all(f(x) == 1 for x in line):
+        ans.append(A2-A1)
+print (max(ans))
