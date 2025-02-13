d80d1c0d36651cf7efbb22070397af70575bbfc3
KZinoveva <kseniazinoveva608@gmail.com>
2025-02-13 17:18:12 +0200

    ответ: 44

---
+++ 13-02-25/Task-6-17519.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+tracer(0)
+screensize(1000,1000)
+lt(90)
+m = 10
+
+for i in range(9):
+    fd(22*m)
+    rt(90)
+    fd(6*m)
+    rt(90)
+up()
+fd(1*m)
+rt(90)
+fd(5*m)
+lt(90)
+down()
+for i in range(9):
+    fd(53*m)
+    rt(90)
+    fd(75*m)
+    rt(90)
+up()
+
+for x in range(-100,100):
+    for y in range(-100, 100):
+        goto(x*m,y*m)
+        dot(3,'white')
+update()
+done()
