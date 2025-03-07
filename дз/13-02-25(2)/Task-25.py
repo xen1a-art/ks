c20cc25693673c5bbcdb9b6f8370ff2ca628253a
User <kseniazinoveva608@gmail.com>
2025-03-07 16:57:00 +0200

    ответ: 121025674 15562 1209805674 155562 1220895676 156988 1231985678 158414 1265395670 162710 1276485672 164136 1287575674 165562 1298665676 166988

--- дз/13-02-25(2)/Task-25.py
+++ дз/13-02-25(2)/Task-25.py
@@ -1,12 +1,13 @@
 from itertools import product
 
 ans = []
-for i in product('0123456789', repeat = 4):
-    i = ''.join(i)
-    for v in '0123456789':
-        num = int('12' + i + '567' + v)
-        if num % 7777 == 0 and sum([num+1])% 2 != 0:
-            ans.append([num,num//7777])
+for r in range(0,5):
+    for i in product('0123456789', repeat = r):
+        i = ''.join(i)
+        for v in '0123456789':
+            num = int('12' + i + '567' + v)
+            if num % 7777 == 0 and sum([num+1])% 2 != 0 and num <= 10**10:
+                ans.append([num,num//7777])
 ans = sorted(ans)
 for i in ans:
     print(*i)
