c0ef318a271e6494cc7f1e03526810a5c842b672
User <kseniazinoveva608@gmail.com>
2025-03-07 16:31:17 +0200

    ответ: 121025674 15562

---
+++ дз/13-02-25(2)/Task-25.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+ans = []
+for i in product('0123456789', repeat = 3):
+    i = ''.join(i)
+    for v in '0123456789':
+        num = int('12' + i + '567' + v)
+        if num % 7777 == 0 and sum([num+1])% 2 != 0:
+            ans.append([num,num//7777])
+ans = sorted(ans)
+for i in ans:
+    print(*i)
