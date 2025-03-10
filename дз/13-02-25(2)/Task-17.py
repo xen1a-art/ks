75185e7136f03f033682219e9b9f468826c5fd31
KZinoveva <kseniazinoveva608@gmail.com>
2025-03-10 16:00:54 +0200

    ответ: 31

--- дз/13-02-25(2)/Task-17.py
+++ дз/13-02-25(2)/Task-17.py
@@ -8,12 +8,14 @@ for i in range(len(data) - 2):
     a1,a2,a3 = data[i], data[i + 1], data[i+2]
     summ = a1+a2+a3
 
-    u1 = 10000 <= a1 <= 99999
-    u2 = 10000 <= a2 <= 99999
-    u3 = 10000 <= a3 <= 99999
+    u1 = 10000 <= abs(a1) <= 99999
+    u2 = 10000 <= abs(a2) <= 99999
+    u3 = 10000 <= abs(a3) <= 99999
 
     f1 = summ <= max_50
-    if f1:
+    f2 = u1 + u2+ u3 == 3
+    f3 = a1 != a2 != a3 != a1
+    if f1 and f2 and f3:
         ans.append(summ)
 print(len(ans),max(ans))
 
