70825fb9e203346e19a9c7a91492cc18025dce6f
KZinoveva <kseniazinoveva608@gmail.com>
2025-03-10 17:54:10 +0200

    ответ:133

---
+++ занятия/10-03-25/Task-23-9844.py
@@ -0,0 +1,8 @@
+def f(cur,end):
+    if cur == end:
+        return 1
+    if cur < end or cur == 7:
+        return 0
+    return f(cur -1, end)+ f(cur - 3, end) + f(cur//2,end)
+
+print(f(19,10)*f(10,3))
