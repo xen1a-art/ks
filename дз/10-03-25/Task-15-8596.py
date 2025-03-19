5d5691d335f498cce8cb78f1b47ec48c32e8476d
User <kseniazinoveva608@gmail.com>
2025-03-19 21:49:27 +0200

    ответ: 301

---
+++ дз/10-03-25/Task-15-8596.py
@@ -0,0 +1,12 @@
+def f(A):
+    for x in range(1,10_000):
+        for y in range(1,10_000):
+            f = (x>=11)or(3*x<y)or(x*y<A)
+            if not f:
+                return False
+    return True
+
+for A in range(1,10_000):
+    if f(A):
+        print(A)
+        break
