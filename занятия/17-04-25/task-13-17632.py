7b386ea5dcf43079d4a34a8fc7495638138b1079
User <kseniazinoveva608@gmail.com>
2025-04-17 16:48:09 +0200

    ответ: 215766

---
+++ занятия/17-04-25/task-13-17632.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network
+net = ip_network('112.160.0.0/255.240.0.0')
+
+cnt = 0
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 5 == 0:
+        cnt += 1
+print(cnt)
