577538d06738bfe46e59da583924354d248b6ca0
KZinoveva <kseniazinoveva608@gmail.com>
2025-04-21 17:43:56 +0200

    ответ: 120

--- занятия/21-04-25/task-13-12451.py
+++ занятия/21-04-25/task-13-12451.py
@@ -1,12 +1,14 @@
 from ipaddress import ip_network, ip_address
 
+cnt = 0
 for A in range(256):
-    ip = ip_address(f'192.214.{A}.184')
+    ip = ip_address(f'246.81.65.{A}')
     net = ip_network(f'{ip}/255.255.255.224',False)
     if ip in net.hosts():
-        for i in net:
+        for i in net.hosts():
             i = f'{int(i):032b}'
-            if not(i.count('1') > 15):
+            if not(i[16:24].count('0') > i[24:].count('0')):
                 break
         else:
-            print(A)
+            cnt += 1
+print(cnt)
