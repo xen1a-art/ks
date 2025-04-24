5d7d416fba178d92145e37978283ac5bbef17b2c
KZinoveva <kseniazinoveva608@gmail.com>
2025-04-24 17:05:48 +0200

    ответ: 20

---
+++ занятия/24-04-25/task-13-14359.py
@@ -0,0 +1,10 @@
+from ipaddress import ip_network, ip_address
+
+for mask in range(0,31)[::-1]:
+    ip1 = ip_address('157.127.172.56')
+    ip2 = ip_address('157.127.191.78')
+    net1 = ip_network(f'{ip1}/{mask}',False )
+    net2 = ip_network(f'{ip2}/{mask}',False )
+    if ip1 in net1.hosts() and ip2 in net2.hosts():
+        if net1.network_address != net2.network_address:
+            print(mask)
