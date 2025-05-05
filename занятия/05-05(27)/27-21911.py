acca526b2f3944d5b205d49c6183228bba52eee1
KZinoveva <kseniazinoveva608@gmail.com>
2025-05-05 17:18:29 +0200

    ответ: 26216 24182 150891 63754

---
+++ занятия/05-05(27)/27-21911.py
@@ -0,0 +1,30 @@
+from math import dist
+
+def centroid(cluster):
+    dists = []
+    for dot in cluster:
+        sum_dist = 0
+        for dot2 in cluster:
+            sum_dist += dist(dot,dot2)
+        dists.append([sum_dist,dot])
+    return min(dists)[1]
+
+with open('27_B_21911.txt') as file:
+    data = [list(map(float, i.split())) for i in file]
+
+clusters = [] # все кластеры
+eps = 2
+while data:
+    cluster = [data.pop()]#pop удаляет и извлекает точку на позиции
+    for dot in cluster:
+        for dot2 in data.copy():
+            if dist(dot,dot2) < eps: # eps расстояние которое мы допускае
+                cluster.append(dot2)
+                data.remove(dot2)# удаляет
+    clusters.append(cluster)
+print([len(cluster) for cluster in clusters])
+
+centers = [centroid(cluster) for cluster in clusters]
+p_x = sum(center[0] for center in centers)/ len(centers)
+p_y = sum(center[1] for center in centers)/ len(centers)
+print(int(p_x * 10_000), int(p_y* 10_000))
