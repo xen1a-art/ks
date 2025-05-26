ac09b41fb44f86ec62142ab6af0a683dfa681cfb
User <kseniazinoveva608@gmail.com>
2025-05-26 10:34:40 +0200

    ответ: 2598

---
+++ занятия/26-05-25/24-21908.py
@@ -0,0 +1,23 @@
+from string import digits, ascii_uppercase
+
+with open('24_21908.txt') as file:
+    data = file.readline()
+
+for i in ascii_uppercase[2:]:
+    data = data.replace(i,' ')
+
+data = data.split()
+
+ans = 0
+for i in data:
+    i = i.strip('0')
+    if i and int(i, 14) % 2 == 0:
+        ans = max(ans, len(i))
+    else:
+        for l in range(len(i)):
+            for r in range(len(i), l, -1):
+                sub_str = i[l:r].strip('0')
+                if sub_str and int(sub_str, 14) % 2 == 0:
+                    ans = max(ans, len(sub_str))
+                    break
+print(ans)
