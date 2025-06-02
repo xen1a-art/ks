0c4b33e3b6ced9f062aa165958f49a1e44249dab
User <kseniazinoveva608@gmail.com>
2025-06-02 19:53:11 +0200

    ответ: z x y w

---
+++ дз (интенсив)/task 2/task-4607.py
@@ -0,0 +1,13 @@
+from itertools import permutations, product
+def f(w,x,y,z):
+    return ((x <= z)<= y) or (not w)
+
+for a1,a2,a3,a4,a5,a6,a7 in product([0,1], repeat = 7):
+    table = [(1,0,a1,a2),
+             (a3,1,0,a4),
+             (0,a5,a6,a7)]
+    if len(table) == len(set(table)):
+        for p in permutations('wxyz'):
+            u = [f(**dict(zip(p,t))) for t in table] == [0,0,0]
+            if u:
+                print(*p)
