ddde3ee38c83586e05a60ae9943cecebf0941d1a
KZinoveva <kseniazinoveva608@gmail.com>
2025-06-02 13:58:28 +0200

    ответ: 166

---
+++ шпора/task5/task-9736.py
@@ -0,0 +1,11 @@
+ans = []
+for n in range(1,10_000):
+    r = bin(n)[2:]
+    if n % 3 == 0:
+        r = r + r[-3:]
+    else:
+        r = r + bin((n % 3)* 3)[2:]
+    r = int(r,2)
+    if r <= 170:
+        ans.append(r)
+print(max(ans))
