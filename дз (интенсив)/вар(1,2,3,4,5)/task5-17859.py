fed779d9996890964850fbfe5fb54ce2abb7b3c0
User <kseniazinoveva608@gmail.com>
2025-06-05 18:13:22 +0200

    ответ: 109

---
+++ дз (интенсив)/вар(1,2,3,4,5)/task5-17859.py
@@ -0,0 +1,11 @@
+ans = []
+for n in range(1,10_000):
+    r = bin(n)[2:]
+    if n % 2 == 0:
+        r = '10' + r
+    else:
+        r = '1' + r + '01'
+    r = int(r,2)
+    if n <= 12:
+        ans.append(r)
+print(max(ans))
