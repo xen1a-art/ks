00c5fbf16e5e26cb3fca4a7f6778d6778c322a3c
User <kseniazinoveva608@gmail.com>
2025-06-07 19:59:44 +0200

    ответ: 2796202

---
+++ дз (интенсив)/вар(11-12-13-14-15)/task13-17676.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network
+
+net = ip_network('115.192.0.0/255.192.0.0')
+cnt = 0
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 3!= 0:
+        cnt += 1
+print(cnt)
