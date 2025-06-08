c65d96240cdb069cd2454341d1249145a19cc188
User <kseniazinoveva608@gmail.com>
2025-06-08 12:41:45 +0200

    ответ: 10738 30730

---
+++ шпора/task27/task-17882(2 раз).py
@@ -0,0 +1,25 @@
+from math import dist
+
+def centroid(cluster):
+    dists = []
+    for dot in cluster:
+        sum_dist = 0
+        for dot2 in cluster:
+            sum_dist += dist(dot,dot2)
+        dists.append([sum_dist,dot])
+    return min(dists)[1]
+
+with open('27_A_17882 (3).txt') as file:
+    cluster_1 = []
+    cluster_2 = []
+    for i in file:
+        x,y = map(float, i.split())
+        if y < 3:
+            cluster_1.append([x,y])
+        else:
+            cluster_2.append([x,y])
+clusters = [cluster_1,cluster_2]
+centers = [centroid(cluster) for cluster in clusters]
+p_x = sum(center[0] for center in centers)/ len(centers)
+p_y = sum(center[1] for center in centers)/len(centers)
+print(int(p_x*10_000),int(p_y*10_000))
