b5f226098bbba332b80251abdcdf57c74fee3a6e
User <kseniazinoveva608@gmail.com>
2025-06-08 13:09:11 +0200

    ответ: a 10592 6300 b 15981 37287

--- шпора/task27/task-20816.py
+++ шпора/task27/task-20816.py
@@ -9,22 +9,23 @@ def centroid(cluster):
         dists.append([sum_dist,dot])
     return min(dists)[1]
 
-with open('27_B_17882 (1).txt') as file:
+with open('27_B_20816.txt') as file:
     cluster_1 = []
     cluster_2 = []
     cluster_3 = []
     for i in file:
         x,y = map(float, i.split())
-        if x > 5:
+        if x < -5:
             cluster_1.append([x,y])
-        elif y < 4:
+        elif y < -5:
             cluster_2.append([x,y])
         else:
             cluster_3.append([x,y])
 
 clusters = [cluster_1,cluster_2,cluster_3]
 centers = [centroid(cluster) for cluster in clusters]
-p_x = sum(center[0] for center in centers)/ len(centers)
+p_x = sum(center[0] for center in centers)/len(centers)
 p_y = sum(center[1] for center in centers)/len(centers)
 print(int(p_x*10_000),int(p_y*10_000))
-# B 37522 51277
+# a 10592 6300
+# b 15981 37287
