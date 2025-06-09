d19c022de528619117d9fefbe848816cdb281f57
User <kseniazinoveva608@gmail.com>
2025-06-09 19:27:22 +0200

    ответ: 699050

---
+++ шпора/task13/task-17526.py
@@ -0,0 +1,10 @@
+from ipaddress import ip_network
+
+net = ip_network('172.16.128.0/255.255.192.0')
+
+cnt = 0
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 2 != 0:
+        cnt+=1
+print(cnt)
